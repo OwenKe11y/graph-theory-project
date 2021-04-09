bf3d7602c9b8c0665f10a8e211f8514f51c1976d
OwenKe11y <owenkellysch@gmail.com>
2021-04-09 14:01:02 +0100

    attempted to rectify the file path bug, no luck

--- script.py
+++ script.py
@@ -3,19 +3,26 @@
 # G00366614@gmit.ie
 import os
 
-"""Ask user for a file path to the text file that will be read"""
+# Load Text
+# Ask user for a file path to the text file that will be read
 filepath = input("Enter the file path to your .txt file: ")
+filepathextension = 'txt'
+os.path.join(filepath, "*."+filepathextension)
 # If the file doesn't exist just ask the user again 
 while not os.path.isfile(filepath):
     print("Whoops! No such file")
     filepath = input("Enter the file path to your .txt file: ")
+
 # Outputs the file path for the user 
 print("You have entered: " + filepath)
 
+# Prints out the file
 print("Outputting the file: ")
 # Open the file and read it
 with open(filepath) as f:
-    for contents in f:
-        print(contents)
+    contents = f.readlines()
+    
 
+# Make the file a single string  
+whole_txt = "".join(contents)
 f.close()
