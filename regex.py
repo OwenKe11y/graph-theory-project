47eff37f9a0c6199abf35fe34556d230e3f28a3d
OwenKe11y <owenkellysch@gmail.com>
2021-04-21 21:00:00 +0100

    Added postfix to nfa function and integrated into the input function

--- regex.py
+++ regex.py
@@ -3,12 +3,32 @@
 # G00366614@gmit.ie
 import os
 
+# Thompsons Construction
+
+
+class State():
+    """A State and its arrows in thompsons construction"""
+    # Constructor
+
+    def __init__(self, label, arrows, accept):
+        """Label is the arrow labels, arrows is a list of states to point to, accept is a boolean as to whether this is an accept state"""
+        self.label = label
+        self.arrows = arrows
+        self.accept = accept
+
+
+class NFA:
+    """A non-deterministic finite automaton"""
+
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
 
 def menu():
     """Displays the menu to the user"""
     print("[1]Load a File")
-    print("[2]Input Infix")
-    print("[3]")
+    print("[2]Input Regular Expression")
     print("[0]Exit program")
 
 
@@ -82,12 +102,90 @@ def shunt(infix):
         stack = stack[:-1]
     return postfix
 
+def re_to_nfa(postfix):
+    # stack for nfas
+    stack = []
+    # loop through postfix r.e left to right
+    for c in postfix:
 
-def infixInput():
-        infix = input("Enter an infix expression ")
-        print(f"Infix: {infix}") 
-        print(f"PostFix: {shunt(infix)}")
-        print()
+        # concatination
+        if c == '.':
+            # pop nfa off the stack
+            nfa2 = stack[-1]
+            stack = stack[:-1]
+            # pop the next nfa off the stack
+            nfa1 = stack[-1]
+            stack = stack[:-1]
+            # make accept state of nfa1 non-accept
+            nfa1.end.accept = False
+            # make it point at the start of nfa2
+            nfa1.end.arrows = [nfa2.start]
+            # make a new nfa with nfa1's start state and nfa2's end state
+            nfa = NFA(nfa1.start, nfa2.end)
+            stack.append(nfa)
+
+        elif c == '|':
+            # pop nfa off the stack
+            nfa2 = stack[-1]
+            stack = stack[:-1]
+            # pop the next nfa off the stack
+            nfa1 = stack[-1]
+            stack = stack[:-1]
+            # Create new start and end states
+            start = State(None, [], False)
+            end = State(None, [], True)
+            # make a new start state point at old start states
+            start.arrows.append(nfa1.start)
+            start.arrows.append(nfa2.start)
+            # make old accept state of nfa1 non-accept
+            nfa1.end.accept = False
+            nfa2.end.accept = False
+            # make a new nfa
+            nfa = NFA(start, end)
+            stack.append(nfa)
+
+        elif c == '*':
+            # pop 1 nfa off the stack
+            nfa1 = stack[-1]
+            stack = stack[:-1]
+            # Create new start and end states
+            start = State(None, [], False)
+            end = State(None, [], True)
+            # make a new start state point at old start states
+            start.arrows.append(nfa1.start)
+            # and at the new end state
+            start.arrows.append(end)
+            # make old accept state non-accept
+            nfa1.end.accept = False
+            # make old accept state point to new accept state
+            nfa1.end.arrows.append(end)
+            # make old accept state point to old accept state
+            nfa1.end.arrows.append(start)
+            # make a new nfa
+            nfa = NFA(start, end)
+            stack.append(nfa)
+        else:
+            # Create an NFA for the non-special character c
+
+            # Create the end state
+            end = State(None, [], True)
+            # Create the start state, pointed at the end state
+            start = State(c, [end], False)
+            # Create the NFA with the start and end State
+            nfa = NFA(start, end)
+            # NFA to the nfa stack
+            stack.append(nfa)
+
+    return stack[0]
+
+def RegInput():
+    infix = input("Enter an infix expression ")
+    postfix = shunt(infix)
+    print(f"Infix: {infix}")
+    print(f"PostFix: {shunt(infix)}")
+    print()
+    print(f"NFA: {postfix}")
+
 
 # Menu Call and option logic
 menu()
@@ -97,12 +195,9 @@ while option != 0:
     if option == 1:
         loadFile()
     elif option == 2:
-        infixInput()
-    elif option == 3:
-        print("This is option 3")
+        RegInput()
     else:
         print("Invalid Option")
-
     print()
     menu()
     option = int(input("Enter your option "))
